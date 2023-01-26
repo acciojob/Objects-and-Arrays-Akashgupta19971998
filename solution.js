f8499158766da896d20a16b175794cf850945895
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-01-26 20:14:31 +0530

    commit

--- solution.js
+++ solution.js
@@ -7,7 +7,7 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team=players
-let team1=team.copyWithin()
+let team1=team.copyWithin(players)
 let cap1=JSON.parse(JSON.stringify(person));
 
 
